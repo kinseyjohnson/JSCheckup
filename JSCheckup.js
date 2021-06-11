1872158f3f8d8a7d80133f2122ef864027355dbf
Kinsey Johnson <yesnikjohnson@gmail.com>
2021-06-11 10:16:11 -0500

    git add changes to js file

---
+++ JSCheckup.js
@@ -0,0 +1,11 @@
+let myName = "Kinsey";
+let currentAge = 25;
+let currentCity = "Indianapolis";
+console.log(`Hi, I'm ${myName} and I am currently ${currentAge}. I live in ${currentCity}.`);
+
+function hello(first, last) {
+    let wholeName = first + " " + last;
+    return wholeName;
+}
+    console.log(hello("Kinsey", "Johnson"));
+
